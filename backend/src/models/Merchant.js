10664f59e20746a53a716322b8e9d67b920820fe
Suad Abrar <suadabrar44@gmail.com>
2026-02-18 08:09:13 +0300

    create schema for marchant model

---
+++ backend/src/models/Merchant.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const merchantSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    businessName: {
+      type: String,
+      required: true,
+    },
+    businessAddress: String,
+    verificationStatus: {
+      type: String,
+      enum: ["pending", "verified", "rejected"],
+      default: "pending",
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+    totalSales: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Merchant", merchantSchema);
