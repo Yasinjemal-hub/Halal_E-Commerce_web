48569f756a0fe89adcb3a0792f4ee1aa7ebb33da
Suad Abrar <suadabrar44@gmail.com>
2026-02-18 08:27:48 +0300

    schema design for certificate model

--- backend/src/models/Merchant.js
+++ backend/src/models/Merchant.js
@@ -5,6 +5,7 @@ const merchantSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+      unique: true,
     },
     businessName: {
       type: String,
